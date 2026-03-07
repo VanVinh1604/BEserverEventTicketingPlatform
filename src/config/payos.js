c73cdcb90760b3130d81679bf07129912cab4772
nhatsang12 <sangtieu891@gmail.com>
2026-03-08 02:04:09 +0700

    add

--- src/config/payos.js
+++ src/config/payos.js
@@ -1,10 +1,9 @@
-const { PayOS } = require("@payos/node");
+const PayOS = require("@payos/node");
 
-// ✅ V2 Constructor - Dùng object thay vì 3 params riêng
-const payos = new PayOS({
-  clientId: process.env.PAYOS_CLIENT_ID,
-  apiKey: process.env.PAYOS_API_KEY,
-  checksumKey: process.env.PAYOS_CHECKSUM_KEY,
-});
+const payos = new PayOS(
+  process.env.PAYOS_CLIENT_ID,
+  process.env.PAYOS_API_KEY,
+  process.env.PAYOS_CHECKSUM_KEY
+);
 
 module.exports = payos;
