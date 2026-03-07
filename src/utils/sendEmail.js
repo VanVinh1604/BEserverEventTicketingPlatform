5da4c32129a5a7922965f842141491a8a2cac0fe
nhatsang12 <sangtieu891@gmail.com>
2026-03-08 04:40:47 +0700

    fix email IPv4

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,24 +1,24 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // 1. Cấu hình trạm trung chuyển (Dùng Gmail)
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4, // force IPv4, fix lỗi ENETUNREACH trên Render
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  // 2. Nội dung bức thư
   const mailOptions = {
     from: '"TicketHub - Nền tảng đặt vé" <no-reply@tickethub.com>',
     to: options.email,
     subject: options.subject,
-    html: options.html, // Chấp nhận gửi cả giao diện HTML cho đẹp
+    html: options.html,
   };
 
-  // 3. Tiến hành gửi
   await transporter.sendMail(mailOptions);
 };
 
