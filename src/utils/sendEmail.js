6f724f46be52dc82a0730e365c15f0661df45257
nhatsang12 <sangtieu891@gmail.com>
2026-03-08 13:38:51 +0700

    fix email IPv4

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,25 +1,15 @@
-const nodemailer = require("nodemailer");
+
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,  // true cho port 465
-    family: 4,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: '"TicketHub - Nền tảng đặt vé" <no-reply@tickethub.com>',
+  await resend.emails.send({
+    from: 'TicketHub <onboarding@resend.dev>',
     to: options.email,
     subject: options.subject,
     html: options.html,
-  };
-
-  await transporter.sendMail(mailOptions);
+  });
 };
 
 module.exports = sendEmail;
