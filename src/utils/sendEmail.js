3200835af4d9791165e04ac29e636b306f2304ed
nhatsang12 <sangtieu891@gmail.com>
2026-03-08 13:06:07 +0700

    fix email IPv4

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,9 +3,9 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4, // force IPv4, fix lỗi ENETUNREACH trên Render
+    port: 465,
+    secure: true,  // true cho port 465
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
